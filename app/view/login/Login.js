3245431970b20911b193b70feabc3a04d38cb7cb
Winniekimani <kimaniwinniew@gmail.com>
2024-03-17 15:30:14 +0300

    adding the red asterik for the required field in the login part

--- app/view/login/Login.js
+++ app/view/login/Login.js
@@ -3,16 +3,24 @@ Ext.define('MsTraining.view.login.Login', {
     xtype: 'login',
     requires: [
         'MsTraining.view.login.LoginController',
-        'Ext.form.Panel'
+        'Ext.form.Panel',
+          'MsTraining.util.Util'
     ],
     controller: 'login',
     bodyPadding: 10,
     title: 'Login Window',
     closable: false,
     autoShow: true,
+    draggable: false,
+     resizable: false,
+     layout: 'fit',
     items: {
             xtype: 'form',
             reference: 'form',
+            defaults: {
+                        afterLabelTextTpl: MsTraining.util.Util.required,
+                    },
+
             items: [{
                 xtype: 'textfield',
                 name: 'username',
@@ -26,11 +34,12 @@ Ext.define('MsTraining.view.login.Login', {
                 fieldLabel: 'Password',
                 allowBlank: false
             },
-            {
+           /* {
                 xtype: 'displayfield',
                 hideEmptyLabel: false,
                 value: 'Enter any non-blank password'
-            }],
+            }*/
+            ],
             buttons: [{
                 text: 'Login',
                 formBind: true,
